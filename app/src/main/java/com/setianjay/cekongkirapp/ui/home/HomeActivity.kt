c663d3a97719230c0cc13d9045e282349c40f641
setianjay <hari.setiaji2000@gmail.com>
2021-04-12 18:28:18 +0700

    Mengimplementasikan view binding pada HomeActivity

--- app/src/main/java/com/setianjay/cekongkirapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/setianjay/cekongkirapp/ui/home/HomeActivity.kt
@@ -6,22 +6,23 @@ import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.setianjay.cekongkirapp.R
+import com.setianjay.cekongkirapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
-    private val tabLayout by lazy { findViewById<TabLayout>(R.id.tab_layout) }
-    private val viewPager by lazy { findViewById<ViewPager2>(R.id.view_pager) }
+    private val binding by lazy { ActivityHomeBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
         setupTabLayout()
     }
 
     private fun setupTabLayout(){
         val tabAdapter = HomeTabAdapter(supportFragmentManager,lifecycle)
         val tabTitles = arrayListOf<String>("Cek Ongkir","Cek Resi")
-        viewPager.adapter = tabAdapter
+        binding.viewPager.adapter = tabAdapter
 
-        TabLayoutMediator(tabLayout,viewPager){tab, position ->
+        TabLayoutMediator(binding.tabLayout,binding.viewPager){tab, position ->
             tab.text = tabTitles[position]
         }.attach()
     }
