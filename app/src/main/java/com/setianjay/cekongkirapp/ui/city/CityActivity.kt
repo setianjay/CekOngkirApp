e56e998ee6507a8362b5935efcbf39aa9da3917e
setianjay <hari.setiaji2000@gmail.com>
2021-04-13 11:17:58 +0700

    Membuat CityActivity beserta komponen tampilan layoutnya

---
+++ app/src/main/java/com/setianjay/cekongkirapp/ui/city/CityActivity.kt
@@ -0,0 +1,12 @@
+package com.setianjay.cekongkirapp.ui.city
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.setianjay.cekongkirapp.R
+
+class CityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city)
+    }
+}
