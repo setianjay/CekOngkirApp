8798175dc548f36ce0495801f0f5b43ececa8a74
setianjay <hari.setiaji2000@gmail.com>
2021-04-13 14:33:41 +0700

    Membuat CityViewModel

---
+++ app/src/main/java/com/setianjay/cekongkirapp/ui/city/CityViewModel.kt
@@ -0,0 +1,8 @@
+package com.setianjay.cekongkirapp.ui.city
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CityViewModel: ViewModel() {
+    val titleBar: MutableLiveData<String> = MutableLiveData("")
+}
