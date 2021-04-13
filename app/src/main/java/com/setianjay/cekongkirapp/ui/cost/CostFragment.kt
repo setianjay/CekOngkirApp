d68b378aece2fab5082e42741262170352762221
setianjay <hari.setiaji2000@gmail.com>
2021-04-13 13:09:52 +0700

    Fix Cost,City,Subdistrict Fragment

--- app/src/main/java/com/setianjay/cekongkirapp/ui/cost/CostFragment.kt
+++ app/src/main/java/com/setianjay/cekongkirapp/ui/cost/CostFragment.kt
@@ -1,21 +1,39 @@
 package com.setianjay.cekongkirapp.ui.cost
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.setianjay.cekongkirapp.R
+import com.setianjay.cekongkirapp.databinding.FragmentCostBinding
+import com.setianjay.cekongkirapp.ui.city.CityActivity
 
 
 class CostFragment : Fragment() {
+    private lateinit var binding: FragmentCostBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cost, container, false)
+        binding = FragmentCostBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+
+    private fun initListener(){
+        binding.etCityOrigin.setOnClickListener {
+            Intent(context,CityActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 
 }
--- app/src/main/java/com/setianjay/cekongkirapp/ui/subdistrict/SubDistrictFragment.kt
+++ app/src/main/java/com/setianjay/cekongkirapp/ui/subdistrict/SubDistrictFragment.kt
@@ -6,17 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.setianjay.cekongkirapp.R
-import com.setianjay.cekongkirapp.databinding.FragmentSubDistrictBinding
+import com.setianjay.cekongkirapp.databinding.FragmentSubdistrictBinding
 
 class SubDistrictFragment : Fragment() {
-    private lateinit var binding: FragmentSubDistrictBinding
+    private lateinit var binding: FragmentSubdistrictBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentSubDistrictBinding.inflate(inflater,container,false)
+        binding = FragmentSubdistrictBinding.inflate(inflater,container,false)
         return binding.root
     }
 }
