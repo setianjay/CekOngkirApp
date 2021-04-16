3a4962c6139b9db2b534a8d35361825992424d21
setianjay <hari.setiaji2000@gmail.com>
2021-04-16 14:30:32 +0700

    Membuat SharedPreferences untuk Cek Ongkir

--- app/src/main/java/com/setianjay/cekongkirapp/constant/Constants.kt
+++ app/src/main/java/com/setianjay/cekongkirapp/constant/Constants.kt
@@ -1,6 +1,14 @@
 package com.setianjay.cekongkirapp.constant
 
 object Constants {
+    // API
     const val BASE_URL = "https://pro.rajaongkir.com/api/"
     const val API_KEY = "4bdfc655c2f1acd34f5e0c2fa4e5cbec"
+
+    // Preferences
+    const val PREF_NAME = "setianjay.cekongkirapp"
+    const val PREF_ORIGIN_ID = "pref_origin_id"
+    const val PREF_ORIGIN_NAME = "pref_origin_name"
+    const val PREF_DESTINATION_ID = "pref_destination_id"
+    const val PREF_DESTINATION_NAME = "pref_destination_name"
 }
---
+++ app/src/main/java/com/setianjay/cekongkirapp/database/preferences/CostPreferences.kt
@@ -0,0 +1,25 @@
+package com.setianjay.cekongkirapp.database.preferences
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.setianjay.cekongkirapp.constant.Constants
+
+class CostPreferences(context: Context) {
+    private val sharedPref = context.getSharedPreferences(Constants.PREF_NAME,Context.MODE_PRIVATE)
+    private val editor: SharedPreferences.Editor
+
+    init {
+        editor = sharedPref.edit()
+    }
+
+    fun putString(key: String, value: String){
+        editor.apply{
+            putString(key,value)
+            apply()
+        }
+    }
+
+    fun getString(key: String): String?{
+        return sharedPref.getString(key,null)
+    }
+}
