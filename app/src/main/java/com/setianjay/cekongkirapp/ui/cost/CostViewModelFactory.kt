b93cb99c748f3191b340ac8516f167d00a03e1cd
setianjay <hari.setiaji2000@gmail.com>
2021-04-19 14:52:00 +0700

    Membuat CityViewModelFactory

---
+++ app/src/main/java/com/setianjay/cekongkirapp/ui/cost/CostViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.setianjay.cekongkirapp.ui.cost
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.setianjay.cekongkirapp.network.repository.RajaOngkirRepository
+
+class CostViewModelFactory(
+    private val repository: RajaOngkirRepository
+): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return CostViewModel(repository) as T
+    }
+
+}
