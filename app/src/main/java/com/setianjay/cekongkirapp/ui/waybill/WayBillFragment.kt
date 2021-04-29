aa4ce454c93f16f6da4a16f50fc0220b085c3518
setianjay <hari.setiaji2000@gmail.com>
2021-04-29 11:41:00 +0700

    Membuat Tracking Navigation

--- app/src/main/java/com/setianjay/cekongkirapp/ui/waybill/WayBillFragment.kt
+++ app/src/main/java/com/setianjay/cekongkirapp/ui/waybill/WayBillFragment.kt
@@ -1,21 +1,35 @@
 package com.setianjay.cekongkirapp.ui.waybill
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.setianjay.cekongkirapp.R
+import com.setianjay.cekongkirapp.databinding.FragmentWayBillBinding
+import com.setianjay.cekongkirapp.ui.tracking.TrackingActivity
 
 
 class WayBillFragment : Fragment() {
+    private lateinit var binding: FragmentWayBillBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_way_bill, container, false)
+        binding = FragmentWayBillBinding.inflate(inflater,container,false )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.etSearch.setOnClickListener {
+            Intent(requireContext(),TrackingActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 
 }
